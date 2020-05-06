504b63788018ffca103535c444a715b7b5c67d94
Guilherme Soares <github@guilhermesoares.tech>
2020-05-06 00:22:04 -0300

    Update content.js
    I developed the functions and added the comments

--- src/content.js
+++ src/content.js
@@ -2,11 +2,23 @@
 //As the "header" class does not load when entering the page, it is necessary to use a check, and search until you find
 const interval = setInterval(() => {
 
-    const header = document.querySelector("._3auIg") //where the button will be
+    const header = document.querySelector("._3auIg") //Where the button will be
 
-    if(header){
-        console.log(header)
-        clearInterval(interval)
+    if (header) { //Check if found the header
+        clearInterval(interval) //stop verification
+
+        const button = document.createElement("Button") //Create a button
+        button.innerHTML = "2x" //This button will display "2x"
+        button.classList.add("TimeButton") //Defines button class
+
+        button.addEventListener("click", () => { //Button click event
+            const audios = document.querySelectorAll("audio"); //Select all áudios on the page
+
+            audios.forEach((audio) => { //Cycle through all audios
+                audio.playbackRate = 1.5; //Sets the audio playback speed
+            })
+        })
+
+        header.appendChild(button) //Add a node to the end of the header list
     }
 }, 1000);
-
